6a8ab51f37e97b9b0cd377a01f3ff9ee6536fb7c
Snehith-1 <130631884+Snehith-1@users.noreply.github.com>
2024-09-11 11:51:42 +0530

    Update buy.php

--- buy.php
+++ buy.php
@@ -1,6 +1,24 @@
 <?php
+// Set the content type to JSON
+header('Content-Type: application/json');
+
+// Decode the JSON POST data
 $post = json_decode($_POST['trackDetails'], true);
-$amt = json_decode($post['details']['tezResponse'], true);
- 
-print_r($post);
+
+// Prepare response data
+$response = [
+  'status' => 'ERROR', // Default status
+  'transactionId' => isset($post['details']['tezResponse']['transactionId']) ? $post['details']['tezResponse']['transactionId'] : 'N/A',
+  'amount' => isset($post['details']['tezResponse']['amount']) ? $post['details']['tezResponse']['amount'] : 'N/A',
+  'message' => 'Payment Failed. Please try again.' // Default message
+];
+
+// Modify response based on the payment status
+if (isset($post['details']['tezResponse']['status']) && $post['details']['tezResponse']['status'] === 'SUCCESS') {
+  $response['status'] = 'SUCCESS';
+  $response['message'] = 'Payment succeeded.';
+}
+
+// Send the response as JSON
+echo json_encode($response);
 ?>
